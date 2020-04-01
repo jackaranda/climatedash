1c0feebcd3a3823737df2ca092fdcb238b7bc5ea
Chris <cjack@csag.uct.ac.za>
2020-04-01 20:14:14 +0200

    Added option to calculate daily deltas

--- app/plot.py
+++ app/plot.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import json
+import copy
 
 import plotly.graph_objects as go
 
@@ -13,19 +15,31 @@ cases = pd.read_json('../data/covid-19/cases.json')
 deaths = pd.read_json('../data/covid-19/deaths.json')
 recovered = pd.read_json('../data/covid-19/recovered.json')
 
-#fig = go.Figure(data=go.Bar(name='cases', x=cases.index, y=cases[query]))
+with open('../data/mapping/ne_50m_admin_0_countries.json') as src:
+	mapdata = json.loads(src.read())
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
 	html.Div([
 		html.H3(children='Covid-19 case dashboard'),
+
+#		dcc.Graph(id='map-plot', figure={}),
+
 	    dcc.Graph(id='timeseries-plot', figure={}),
+   	    
    	    dcc.RadioItems(
 	    	id='log-scale',
 	    	options=[{'label':'linear','value':'linear'},{'label':'logarithmic','value':'log'}],
 	    	value='linear',
 	    	style={'width':300}),
+	    
+   	    dcc.RadioItems(
+	    	id='delta',
+	    	options=[{'label':'cumulative','value':'cumulative'},{'label':'delta','value':'delta'}],
+	    	value='cumulative',
+	    	style={'width':300}),
+
 	    html.Div([
 		    dcc.Dropdown(
 		    	id='country-select',
@@ -47,39 +61,63 @@ app.layout = html.Div([
 	className='container')
 ])
 
+
+timeseries_plot = {
+	'data':[
+		{'type':'bar', 'marker':{'color':'rgb(128,128,128)','alpha':0.7}, 'name':'cases'},
+		{'type':'bar', 'marker':{'color':'rgb(64,64,64)'}, 'name':'deaths'},
+		{'type':'bar', 'marker':{'color':'rgb(64,256,64)'}, 'name':'recovered'}
+	],
+	'layout':{
+		'barmode':'stack',
+		'yaxis':{'type':'log'}
+	}
+}
+
+
 @app.callback(
 	dash.dependencies.Output('timeseries-plot', 'figure'),
 	[dash.dependencies.Input('country-select', 'value'),
 	 dash.dependencies.Input('country-compare', 'value'),
-	 dash.dependencies.Input('log-scale', 'value')])
-def update_timeseries_plot(select, compare, scale):
+	 dash.dependencies.Input('log-scale', 'value'),
+	 dash.dependencies.Input('delta', 'value')])
+def update_timeseries_plot(select, compare, scale, delta):
 
-	result = {
-		'data':[
-			{'x':cases[select].index, 'y':cases[select], 
-				'type':'bar', 'marker':{'color':'rgb(128,128,128)','alpha':0.7}, 'name':'cases'},
-			{'x':deaths[select].index, 'y':deaths[select], 
-				'type':'bar', 'marker':{'color':'rgb(64,64,64)'}, 'name':'deaths'},
-			{'x':recovered[select].index, 'y':recovered[select], 
-				'type':'bar', 'marker':{'color':'rgb(64,256,64)'}, 'name':'recovered'}
-		],
-		'layout':{
-			'barmode':'stack',
-			'yaxis':{'type':'log'}
-		}
-	}
+	plot = copy.copy(timeseries_plot)
+
+	if delta == 'cumulative':
+		plot['data'][0]['x'] = cases[select].index
+		plot['data'][0]['y'] = cases[select]
+
+		plot['data'][1]['x'] = deaths[select].index
+		plot['data'][1]['y'] = deaths[select]
+
+		plot['data'][2]['x'] = recovered[select].index
+		plot['data'][2]['y'] = recovered[select]
+
+	else:
+		plot['data'][0]['x'] = cases[select].index[1:]
+		plot['data'][0]['y'] = cases[select].diff()
+
+		plot['data'][1]['x'] = deaths[select].index[1:]
+		plot['data'][1]['y'] = deaths[select].diff()
+
+		plot['data'][2]['x'] = recovered[select].index[1:]
+		plot['data'][2]['y'] = recovered[select].diff()
+
 
 	if scale != 'log':
-		result['layout']['yaxis']['type'] = 'linear'
+		plot['layout']['yaxis']['type'] = 'linear'
 
 	if len(compare):
 		for country in compare:
-			result['data'].append(
+			plot['data'].append(
 				{'x':cases[country].index, 'y':cases[country], 
 					'type':'line', 'marker':{'color':'rgb(256,128,128)', 'line-width':40}, 'name':country}
 				)
 
-	return result
+	return plot
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
